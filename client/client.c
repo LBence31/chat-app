c0b5f834680725418c1b8abc0cd727deb43d6fc8
LBence31 <belga.1212@gmail.com>
2026-01-12 23:36:41 +0100

    Server without encapsulation and recv and send check

---
+++ client/client.c
@@ -0,0 +1,14 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    int nbytes;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    return 0;
+}
--- server/server.c
+++ server/server.c
@@ -1,7 +1,9 @@
+#include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <ctype.h>
 #include <limits.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -102,8 +104,134 @@ int get_listener_socket(char *port)
     return listener;
 }
 
+void add_to_pfds(struct pollfd **pfds, int newfd, int *fd_count, int *fd_size)
+{
+    if (*fd_count == *fd_size)
+    {
+        *fd_size *= 2;
+        *pfds = realloc(*pfds, sizeof(**pfds) * (*fd_size));
+    }
+
+    (*pfds)[*fd_count].fd = newfd;
+    (*pfds)[*fd_count].events = POLLIN;
+    (*pfds)[*fd_count].revents = 0;
+
+    (*fd_count)++;
+}
+
+void del_from_pfds(struct pollfd *pfds, int pfd_i, int *fd_count)
+{
+    pfds[pfd_i] = pfds[*fd_count - 1];
+    (*fd_count)--;
+}
+
+const char *inet_ntop2(void *addr, char *buf, size_t size)
+{
+    struct sockaddr_storage *sas = addr;
+    struct sockaddr_in *sa4;
+    struct sockaddr_in6 *sa6;
+    void *src;
+
+    switch (sas->ss_family)
+    {
+    case AF_INET:
+        sa4 = addr;
+        src = &(sa4->sin_addr);
+        break;
+    case AF_INET6:
+        sa6 = addr;
+        src = &(sa6->sin6_addr);
+        break;
+    default:
+        return NULL;
+    }
+
+    return inet_ntop(sas->ss_family, src, buf, size);
+}
+
+void handle_new_connection(int listener, int *fd_count, int *fd_size, struct pollfd **pfds)
+{
+    struct sockaddr_storage remoteaddr;
+    socklen_t addrlen;
+    int newfd;
+    char remoteIP[INET6_ADDRSTRLEN];
+
+    addrlen = sizeof remoteaddr;
+    newfd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen);
+
+    if (newfd == -1)
+    {
+        perror("accept");
+    }
+    else
+    {
+        add_to_pfds(pfds, newfd, fd_count, fd_size);
+
+        printf("pollserver: new connection from %s on socket %d\n", inet_ntop2(&remoteaddr, remoteIP, sizeof remoteIP),
+               newfd);
+    }
+}
+
+void handle_client_data(int listener, int *fd_count, struct pollfd *pfds, int *pfd_i)
+{
+    char buf[256];
+
+    int nbytes = recv(pfds[*pfd_i].fd, buf, sizeof buf, 0);
+
+    int sender_fd = pfds[*pfd_i].fd;
+
+    if (nbytes <= 0)
+    {
+        if (nbytes == 0)
+        {
+            printf("pollserver: socket %d hung up\n", sender_fd);
+        }
+        else
+        {
+            perror("recv");
+        }
+
+        close(pfds[*pfd_i].fd);
+        del_from_pfds(pfds, *pfd_i, fd_count);
+        (*pfd_i)--;
+    }
+    else
+    {
+        printf("pollserver: recv from fd %d: %.*s", sender_fd, nbytes, buf);
+        for (int i = 0; i < *fd_count; i++)
+        {
+            int dest_fd = pfds[i].fd;
+
+            if (dest_fd != listener && dest_fd != sender_fd)
+            {
+                if (send(dest_fd, buf, nbytes, 0) == -1)
+                {
+                    perror("send");
+                }
+            }
+        }
+    }
+}
+
 void process_connections(int listener, int *fd_count, int *fd_size, struct pollfd **pfds)
 {
+    for (int i = 0; i < *fd_count; i++)
+    {
+
+        if ((*pfds)[i].revents & (POLLIN | POLLHUP))
+        {
+
+            if ((*pfds)[i].fd == listener)
+            {
+                handle_new_connection(listener, fd_count, fd_size, pfds);
+            }
+
+            else
+            {
+                handle_client_data(listener, fd_count, *pfds, &i);
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[])
